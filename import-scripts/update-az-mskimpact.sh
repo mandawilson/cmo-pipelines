5380f903d384b426a245fffbc37d73588c55c4fc
Chennault <chennac@mskcc.org>
2022-11-30 11:48:39 -0500

    Add script file with pipeline steps

---
+++ import-scripts/update-az-mskimpact.sh
@@ -0,0 +1,9 @@
+# 1. Pull latest from AstraZeneca repo (mskcc/az-msk-impact-2022)
+
+# 2. Copy data from local clone of MSK Solid Heme repo to local clone of AZ repo
+
+# 3. Remove Part C non-consented patients + samples (use python2)
+
+# 4. Run changelog script (use python3)
+
+# 5. Push the updates data to GitHub
